fc89272f8a9550e745bd4e8e57fa8fae84d0c932
denetor <denetor@gmail.com>
2019-11-23 15:22:19 +0100

    removed multiconverter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NdrNavbarComponent } from './ndr-navbar/ndr-navbar.component';
-import { NdrMulticonverterComponent } from './ndr-multiconverter/ndr-multiconverter.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NdrNavbarComponent, NdrMulticonverterComponent ],
+  declarations: [ AppComponent, HelloComponent, NdrNavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
